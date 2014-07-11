4e29c9281f423b8221d05791855fadcea7848302
Stuart Wheater <stuart.wheater@arjuna.com>
2014-07-11 12:16:29 +0100

    Create JavaDocs and New tests (incomplete)

---
+++ metadata-rdf/src/test/java/com/arjuna/databroker/tests/metadata/rdf/NavigationToMetadataContentTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2013-2014, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.databroker.tests.metadata.rdf;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.arjuna.databroker.metadata.Metadata;
+import com.arjuna.databroker.metadata.MetadataInventory;
+import com.arjuna.databroker.metadata.rdf.InMemoryBlobMetadataInventory;
+import com.arjuna.databroker.metadata.rdf.InMemoryBlobMutableMetadataInventory;
+import com.arjuna.databroker.metadata.selectors.MetadataSelector;
+import com.arjuna.databroker.metadata.selectors.MetadatasSelector;
+
+public class NavigationToMetadataContentTest
+{
+    @BeforeClass
+    public static void setupInventory()
+    {
+        InMemoryBlobMetadataInventory rdfMetadataInventory = new InMemoryBlobMetadataInventory();
+
+        InMemoryBlobMutableMetadataInventory inMemoryBlobMutableMetadataInventory = rdfMetadataInventory.mutableClone(InMemoryBlobMutableMetadataInventory.class);
+        inMemoryBlobMutableMetadataInventory.createRootMetadata("id", "RDF text", null);
+        
+        
+    }
+
+    @Test
+    public void inventoryToMetadata()
+    {
+        assertNotNull("Not expecting null RDF Metadata Inventory object", _rdfMetadataInventory);
+
+        MetadataSelector metadataSelector = _rdfMetadataInventory.metadata("id");
+        assertNotNull("Not expecting null Metadata Selector object", metadataSelector);
+
+        Metadata metadata = metadataSelector.getMetadata();
+        assertNotNull("Not expecting null Metadata object", metadata);
+    }
+
+    @Test
+    public void inventoryToMetadataIndirect()
+    {
+        assertNotNull("Not expecting null RDF Metadata Inventory object", _rdfMetadataInventory);
+
+        MetadatasSelector metadatasSelector = _rdfMetadataInventory.metadatas();
+        assertNotNull("Not expecting null Metadatas Selector object", metadatasSelector);
+
+        MetadataSelector metadataSelector = metadatasSelector.metadata("id");
+        assertNotNull("Not expecting null Metadata Selector object", metadataSelector);
+
+        Metadata metadata = metadataSelector.getMetadata();
+        assertNotNull("Not expecting null Metadata object", metadata);
+    }
+
+    private static Metadata _rdfMetadata;
+}
