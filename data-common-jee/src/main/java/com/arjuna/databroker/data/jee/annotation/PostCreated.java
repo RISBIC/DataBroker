b627da4095e2e1230e62076dc96e2773fe9e8f1f
Stuart Wheater <stuart.wheater@arjuna.com>
2014-08-21 15:46:10 +0100

    Addition of 'data-common-jee'

---
+++ data-common-jee/src/main/java/com/arjuna/databroker/data/jee/annotation/PostCreated.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2013-2014, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.databroker.data.jee.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PostCreated
+{
+}
---
+++ data-common-jee/src/main/java/com/arjuna/databroker/data/jee/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2013-2014, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+/**
+ * Provides the interfaces necessary to interact with the data level of the data broker.
+ */
+
+package com.arjuna.databroker.data.jee;
