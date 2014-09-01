94e4e1289179c3bc18057c493b17820b319e1524
Stuart Wheater <stuart.wheater@arjuna.com>
2014-09-01 22:42:58 +0100

    Conditional metadata DB initilization

--- metadata-store/src/main/java/com/arjuna/databroker/metadata/store/StoreSetup.java
+++ metadata-store/src/main/java/com/arjuna/databroker/metadata/store/StoreSetup.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.ejb.TransactionAttribute;
@@ -32,27 +33,30 @@ public class StoreSetup implements Serializable
     @PostConstruct
     public void setup()
     {
-        MetadataEntity      descriptionSchemaMetadata                 = new MetadataEntity("DescriptionSchema", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/DescriptionSchema.rdf"));
-        MetadataEntity      dataSourceSchemaMetadata                  = new MetadataEntity("DataSourceSchema", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/DataSourceSchema.rdf"));
-        MetadataEntity      accessSchemaMetadata                      = new MetadataEntity("AccessSchema", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/AccessSchema.rdf"));
-        MetadataEntity      speedManagementNetworkBeforeMetadata      = new MetadataEntity("SpeedManagementNetwork_before", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/SpeedManagementNetwork_before.rdf"));
-        MetadataEntity      speedManagementNetworkAfterMetadata       = new MetadataEntity("SpeedManagementNetwork_after", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/SpeedManagementNetwork_after.rdf"));
-        AccessControlEntity descriptionSchemaAccessControl            = new AccessControlEntity(descriptionSchemaMetadata, null, null, false, true, false, false, false, false);
-        AccessControlEntity dataSourceSchemaAccessControl             = new AccessControlEntity(dataSourceSchemaMetadata, null, null, false, true, false, false, false, false);
-        AccessControlEntity accessSchemaAccessControl                 = new AccessControlEntity(accessSchemaMetadata, null, null, false, true, false, false, false, false);
-        AccessControlEntity speedManagementNetworkBeforeAccessControl = new AccessControlEntity(speedManagementNetworkBeforeMetadata, null, null, true, true, true, true, true, true);
-        AccessControlEntity speedManagementNetworkAfterAccessControl  = new AccessControlEntity(speedManagementNetworkAfterMetadata, null, null, true, true, true, true, true, true);
+        if (_metadataUtils.getIds().size() == 0)
+        {
+            MetadataEntity      descriptionSchemaMetadata                 = new MetadataEntity("DescriptionSchema", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/DescriptionSchema.rdf"));
+            MetadataEntity      dataSourceSchemaMetadata                  = new MetadataEntity("DataSourceSchema", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/DataSourceSchema.rdf"));
+            MetadataEntity      accessSchemaMetadata                      = new MetadataEntity("AccessSchema", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/AccessSchema.rdf"));
+            MetadataEntity      speedManagementNetworkBeforeMetadata      = new MetadataEntity("SpeedManagementNetwork_before", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/SpeedManagementNetwork_before.rdf"));
+            MetadataEntity      speedManagementNetworkAfterMetadata       = new MetadataEntity("SpeedManagementNetwork_after", null, null, loadRDFResource("com/arjuna/databroker/metadata/store/SpeedManagementNetwork_after.rdf"));
+            AccessControlEntity descriptionSchemaAccessControl            = new AccessControlEntity(descriptionSchemaMetadata, null, null, false, true, false, false, false, false);
+            AccessControlEntity dataSourceSchemaAccessControl             = new AccessControlEntity(dataSourceSchemaMetadata, null, null, false, true, false, false, false, false);
+            AccessControlEntity accessSchemaAccessControl                 = new AccessControlEntity(accessSchemaMetadata, null, null, false, true, false, false, false, false);
+            AccessControlEntity speedManagementNetworkBeforeAccessControl = new AccessControlEntity(speedManagementNetworkBeforeMetadata, null, null, true, true, true, true, true, true);
+            AccessControlEntity speedManagementNetworkAfterAccessControl  = new AccessControlEntity(speedManagementNetworkAfterMetadata, null, null, true, true, true, true, true, true);
 
-        _entityManager.persist(descriptionSchemaMetadata);
-        _entityManager.persist(dataSourceSchemaMetadata);
-        _entityManager.persist(accessSchemaMetadata);
-        _entityManager.persist(speedManagementNetworkBeforeMetadata);
-        _entityManager.persist(speedManagementNetworkAfterMetadata);
-        _entityManager.persist(descriptionSchemaAccessControl);
-        _entityManager.persist(dataSourceSchemaAccessControl);
-        _entityManager.persist(accessSchemaAccessControl);
-        _entityManager.persist(speedManagementNetworkBeforeAccessControl);
-        _entityManager.persist(speedManagementNetworkAfterAccessControl);
+            _entityManager.persist(descriptionSchemaMetadata);
+            _entityManager.persist(dataSourceSchemaMetadata);
+            _entityManager.persist(accessSchemaMetadata);
+            _entityManager.persist(speedManagementNetworkBeforeMetadata);
+            _entityManager.persist(speedManagementNetworkAfterMetadata);
+            _entityManager.persist(descriptionSchemaAccessControl);
+            _entityManager.persist(dataSourceSchemaAccessControl);
+            _entityManager.persist(accessSchemaAccessControl);
+            _entityManager.persist(speedManagementNetworkBeforeAccessControl);
+            _entityManager.persist(speedManagementNetworkAfterAccessControl);
+        }
     }
 
     private String loadRDFResource(String rdfResourceName)
@@ -82,4 +86,7 @@ public class StoreSetup implements Serializable
 
     @PersistenceContext(unitName="Metadata")
     private EntityManager _entityManager;
+    
+    @EJB
+    private MetadataUtils _metadataUtils;
 }
