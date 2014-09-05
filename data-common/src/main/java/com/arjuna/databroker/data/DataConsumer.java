a52cf9bdfe6b9fd4f99a07f90d9a3e32ed11a212
Stuart Wheater <stuart.wheater@arjuna.com>
2014-09-05 14:52:24 +0100

    Moved consume

--- data-common/src/main/java/com/arjuna/databroker/data/DataConsumer.java
+++ data-common/src/main/java/com/arjuna/databroker/data/DataConsumer.java
@@ -15,12 +15,4 @@ public interface DataConsumer<T>
      * @return the data flow node associated with the data consumer
      */
     public DataFlowNode getDataFlowNode();
-
-    /**
-     * Cause the consuming of a data item by the associated data flow node.
-     * 
-     * @param dataProvider the data provider which has produced the data
-     * @param data the data to be consumed
-     */
-    public void consume(DataProvider<T> dataProvider, T data);
 }
--- data-common/src/main/java/com/arjuna/databroker/data/connector/ObserverDataConsumer.java
+++ data-common/src/main/java/com/arjuna/databroker/data/connector/ObserverDataConsumer.java
@@ -5,10 +5,18 @@
 package com.arjuna.databroker.data.connector;
 
 import com.arjuna.databroker.data.DataConsumer;
+import com.arjuna.databroker.data.DataProvider;
 
 /**
  * ObserverDataConsumer is an interface to an observer data consumer.
  */
 public interface ObserverDataConsumer<T> extends DataConsumer<T>
 {
+    /**
+     * Cause the consuming of a data item by the associated data flow node.
+     * 
+     * @param dataProvider the data provider which has produced the data
+     * @param data the data to be consumed
+     */
+    public void consume(DataProvider<T> dataProvider, T data);
 }
