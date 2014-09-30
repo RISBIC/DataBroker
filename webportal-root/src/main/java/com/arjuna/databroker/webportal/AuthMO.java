10b0ff9c795c03019a3db01f95b3b615e9ca4812
Stuart Wheater <stuart.wheater@arjuna.com>
2014-09-30 10:41:28 +0100

    Fixed signout (home page only) and signout error

--- webportal-root/src/main/java/com/arjuna/databroker/webportal/AuthMO.java
+++ webportal-root/src/main/java/com/arjuna/databroker/webportal/AuthMO.java
@@ -4,8 +4,11 @@
 
 package com.arjuna.databroker.webportal;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.security.Principal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -16,6 +19,8 @@ public class AuthMO implements Serializable
 {
     private static final long serialVersionUID = -8233388832031549211L;
 
+    private static final Logger logger = Logger.getLogger(AuthMO.class.getName());
+
     public AuthMO()
     {
     }
@@ -24,6 +29,8 @@ public class AuthMO implements Serializable
     {
         final FacesContext facesContext = FacesContext.getCurrentInstance();
         final Principal    principal    = facesContext.getExternalContext().getUserPrincipal();
+
+        logger.log(Level.FINE, "getUsername: " + principal);
 
         if (principal != null)
             return principal.getName();
@@ -51,10 +58,13 @@ public class AuthMO implements Serializable
     }
 
     public String doSignout()
+        throws IOException
     {
         final FacesContext facesContext = FacesContext.getCurrentInstance();
 
         facesContext.getExternalContext().invalidateSession();
+
+        facesContext.getExternalContext().redirect("/index.html");
 
         return "/index?faces-redirect=true";
     }
