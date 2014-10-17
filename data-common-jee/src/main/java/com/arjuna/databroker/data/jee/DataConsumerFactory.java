e8b7bb44035d3dd9d753e5771ba5c402ff57f7bf
Martyn Taylor <mtaylor@redhat.com>
2014-10-17 15:49:01 +0100

    more work

--- data-common-jee/src/main/java/com/arjuna/databroker/data/jee/DataConsumerFactory.java
+++ data-common-jee/src/main/java/com/arjuna/databroker/data/jee/DataConsumerFactory.java
@@ -4,12 +4,14 @@
 
 package com.arjuna.databroker.data.jee;
 
+import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.arjuna.databroker.data.DataConsumer;
 import com.arjuna.databroker.data.DataFlowNode;
 import com.arjuna.databroker.data.connector.ObserverDataConsumer;
 import com.arjuna.databroker.data.connector.ReferrerDataConsumer;
+import org.risbic.transport.jms.JMSDataConsumerImpl;
 
 public class DataConsumerFactory
 {
@@ -29,10 +31,13 @@ public class DataConsumerFactory
         return new DefaultObserverDataConsumer<T>(dataFlowNode, methodName, dataClass);
     }
 
-    public static <T> ReferrerDataConsumer<T> createReferrerDataConsumer(DataFlowNode dataFlowNode, String methodName, Class<T> dataClass)
+    public static <T> ReferrerDataConsumer<T> createReferrerDataConsumer(DataFlowNode dataFlowNode, String methodName, Class<T> dataClass) throws Exception
     {
-        logger.log(Level.FINE, "createNamedDataProvider");
-
-        return null;
+        if (Serializable.class.isAssignableFrom(dataClass))
+        {
+           logger.log(Level.FINE, "createNamedDataProvider");
+           return new JMSDataConsumerImpl(dataFlowNode, methodName);
+        }
+        throw new RuntimeException("Unsupported Data Class: " + dataClass);
     }
 }
