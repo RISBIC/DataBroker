77cffc1fe92cb2ef34bd7ed5db11e27920651f74
Stuart Wheater <stuart.wheater@arjuna.com>
2014-10-29 12:44:54 +0000

    Test of Object View Methods

---
+++ metadata-rdf/src/test/java/com/arjuna/databroker/tests/metadata/rdf/ObjectMethodViewTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2013-2014, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.databroker.tests.metadata.rdf;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.arjuna.databroker.metadata.Metadata;
+import com.arjuna.databroker.metadata.MetadataContent;
+import com.arjuna.databroker.metadata.MetadataInventory;
+import com.arjuna.databroker.metadata.rdf.StoreMetadataInventory;
+import com.arjuna.databroker.metadata.rdf.selectors.RDFMetadataContentsSelector;
+
+public class ObjectMethodViewTest
+{
+    @BeforeClass
+    public static void setupInventory()
+    {
+        try
+        {
+            List<String>              ids              = new LinkedList<String>();
+            Map<String, String>       contentMap       = new HashMap<String, String>();
+            Map<String, String>       descriptionIdMap = new HashMap<String, String>();
+            Map<String, String>       parentIdMap      = new HashMap<String, String>();
+            Map<String, List<String>> childrenIdsMap   = new HashMap<String, List<String>>();
+
+            String test0001 = Utils.loadInputStream(MetaContentToViewTest.class.getResourceAsStream("Test0001.rdf"));
+
+            ids.add("id1");
+            contentMap.put("id1", test0001);
+
+            DummyMetadataContentStore dummyMetadataContentStore = new DummyMetadataContentStore(ids, contentMap, descriptionIdMap, parentIdMap, childrenIdsMap);
+            MetadataInventory         metadataInventory         = new StoreMetadataInventory(dummyMetadataContentStore);
+            Metadata                  metadata                  = metadataInventory.metadata("id1").getMetadata();
+
+            _metadataContent = metadata.contents().selector(RDFMetadataContentsSelector.class).withPath("http://rdf.arjuna.com/test0001#Test01").getMetadataContent();
+        }
+        catch (Throwable throwable)
+        {
+            fail("Failed to populate Metadata Inventory");
+        }
+    }
+
+    @Test
+    public void toStringMethod()
+    {
+        TestView testView = _metadataContent.getView(TestView.class);
+        assertNotNull("Not expecting null Test View object", testView);
+
+        String stringForm = testView.toString();
+        assertNotNull("Not expecting null for string form Test View object", stringForm);
+    }
+
+    private static MetadataContent _metadataContent;
+}
