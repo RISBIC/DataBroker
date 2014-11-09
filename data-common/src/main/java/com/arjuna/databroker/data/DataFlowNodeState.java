92bb6af5aaaf459f8d25a10bf081047a50a4d210
Stuart Wheater <stuart.wheater@arjuna.com>
2014-11-09 18:57:32 +0000

    Initial durable data flow node API support

---
+++ data-common/src/main/java/com/arjuna/databroker/data/DataFlowNodeState.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2013-2014, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.databroker.data;
+
+import java.io.Serializable;
+
+/**
+ * DataFlowNodeState is an interface to a data flow node state.
+ */
+public interface DataFlowNodeState
+{
+    /**
+     * Returns the state associated with the of data flow node.
+     * 
+     * @return the state associated with the of data flow node.
+     */
+    public Serializable getState();
+
+    /**
+     * Sets the state associated with the of data flow node.
+     * 
+     * @param state the state associated with the of data flow node.
+     */
+    public void setState(Serializable state);
+}
