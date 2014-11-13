77bdb4d6e19c00960ef034ab6673bedbd19bb331
Mark Turner <mark.turner@ncl.ac.uk>
2014-11-13 09:14:54 +0000

    Fixed link to data flow creation Added user count to users header Removed add submenu item for users and data brokers lists

--- webportal-root/src/main/java/com/arjuna/databroker/webportal/NavigationMO.java
+++ webportal-root/src/main/java/com/arjuna/databroker/webportal/NavigationMO.java
@@ -22,8 +22,8 @@ public class NavigationMO implements Serializable
         _locations = new LinkedList<LocationVO>();
 
         _locations.add(new LocationVO("Home", "/index?faces-redirect=true", "fa fa-home"));
-        _locations.add(new LocationVO("Search", "/search/searchhome?faces-redirect=true", "fa fa-search"));
-        _locations.add(new LocationVO("Data Flows", "/create/createhome?faces-redirect=true", "fa fa-puzzle-piece"));
+        _locations.add(new LocationVO("Search", "/dataviews/dataviews?faces-redirect=true", "fa fa-search"));
+        _locations.add(new LocationVO("Data Flows", "/dataflows/dataflows?faces-redirect=true", "fa fa-puzzle-piece"));
         _locations.add(new LocationVO("Config", "/users/users?faces-redirect=true", "fa fa-cog"));
     }
 
