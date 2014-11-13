cac6479103e8a52ff847980305e7ecc1354ca495
Stuart Wheater <stuart.wheater@arjuna.com>
2014-11-13 07:56:17 +0000

    Update NamedDataProvider and ReferrerDataConsumer interface

--- data-common/src/main/java/com/arjuna/databroker/data/connector/ReferrerDataConsumer.java
+++ data-common/src/main/java/com/arjuna/databroker/data/connector/ReferrerDataConsumer.java
@@ -11,4 +11,27 @@ import com.arjuna.databroker.data.DataConsumer;
  */
 public interface ReferrerDataConsumer<T> extends DataConsumer<T>
 {
+    /**
+     * Gets name class associated with the Referrer Data Consumer.
+     * 
+     * @param nameClass the class of the name required
+     *
+     * @return name associated with the Referrer Data Consumer
+     */
+
+	public Class<?> getNameClass();
+
+	/**
+     * Add name of named data provider to referrer data consumer.
+     * 
+     * @param name of the named data provider
+     */
+    public <N> void addReferredName(N name);
+
+	/**
+     * Remove name of named data provider from referrer data consumer.
+     * 
+     * @param name of the named data provider
+     */
+    public <N> void removeReferredName(N name);
 }
