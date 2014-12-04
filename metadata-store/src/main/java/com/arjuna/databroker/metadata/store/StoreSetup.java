f8c97e101493dcb1eb359723fea373182c7c9d1a
Stuart Wheater <stuart.wheater@arjuna.com>
2014-12-04 00:36:49 +0000

    Addition of 'BusinessData.rdf'

--- metadata-store/src/main/java/com/arjuna/databroker/metadata/store/StoreSetup.java
+++ metadata-store/src/main/java/com/arjuna/databroker/metadata/store/StoreSetup.java
@@ -44,22 +44,26 @@ public class StoreSetup implements Serializable
             MetadataEntity      accessSchemaMetadata                      = new MetadataEntity(UUID.randomUUID().toString(), null, null, loadRDFResource("com/arjuna/databroker/metadata/store/AccessSchema.rdf"));
             MetadataEntity      speedManagementNetworkBeforeMetadata      = new MetadataEntity(UUID.randomUUID().toString(), null, null, loadRDFResource("com/arjuna/databroker/metadata/store/SpeedManagementNetwork_before.rdf"));
             MetadataEntity      speedManagementNetworkAfterMetadata       = new MetadataEntity(UUID.randomUUID().toString(), null, null, loadRDFResource("com/arjuna/databroker/metadata/store/SpeedManagementNetwork_after.rdf"));
+            MetadataEntity      businessDataMetadata                      = new MetadataEntity(UUID.randomUUID().toString(), null, null, loadRDFResource("com/arjuna/databroker/metadata/store/BusinessData.rdf"));
             AccessControlEntity descriptionSchemaAccessControl            = new AccessControlEntity(descriptionSchemaMetadata, null, null, false, true, false, false, false, false);
             AccessControlEntity dataSourceSchemaAccessControl             = new AccessControlEntity(dataSourceSchemaMetadata, null, null, false, true, false, false, false, false);
             AccessControlEntity accessSchemaAccessControl                 = new AccessControlEntity(accessSchemaMetadata, null, null, false, true, false, false, false, false);
             AccessControlEntity speedManagementNetworkBeforeAccessControl = new AccessControlEntity(speedManagementNetworkBeforeMetadata, null, null, true, true, true, true, true, true);
             AccessControlEntity speedManagementNetworkAfterAccessControl  = new AccessControlEntity(speedManagementNetworkAfterMetadata, null, null, true, true, true, true, true, true);
+            AccessControlEntity businessDataAccessControl                 = new AccessControlEntity(businessDataMetadata, null, null, true, true, true, true, true, true);
 
             _entityManager.persist(descriptionSchemaMetadata);
             _entityManager.persist(dataSourceSchemaMetadata);
             _entityManager.persist(accessSchemaMetadata);
             _entityManager.persist(speedManagementNetworkBeforeMetadata);
             _entityManager.persist(speedManagementNetworkAfterMetadata);
+            _entityManager.persist(businessDataMetadata);
             _entityManager.persist(descriptionSchemaAccessControl);
             _entityManager.persist(dataSourceSchemaAccessControl);
             _entityManager.persist(accessSchemaAccessControl);
             _entityManager.persist(speedManagementNetworkBeforeAccessControl);
             _entityManager.persist(speedManagementNetworkAfterAccessControl);
+            _entityManager.persist(businessDataAccessControl);
         }
     }
 
