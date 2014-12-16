20143deb6ef89cdcea748035372b77af7af3ce69
Stuart Wheater <stuart.wheater@arjuna.com>
2014-12-16 09:42:52 +0000

    Initital skeleton for JPA based durability of data flows

---
+++ data-common-jee-store/src/main/java/com/arjuna/databroker/data/jee/store/DataFlowNodeUtils.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2013-2014, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.databroker.data.jee.store;
+
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+@TransactionManagement(TransactionManagementType.CONTAINER)
+public class DataFlowNodeUtils
+{
+    private static final Logger logger = Logger.getLogger(DataFlowNodeUtils.class.getName());
+
+    @PersistenceContext(unitName="DataFlow")
+    private EntityManager _entityManager;
+}
