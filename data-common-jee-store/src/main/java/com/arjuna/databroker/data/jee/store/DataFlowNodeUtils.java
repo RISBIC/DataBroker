0bc5eb9702167468f461986e71b9216457d71a3b
Stuart Wheater <stuart.wheater@arjuna.com>
2014-12-17 16:03:41 +0000

    Integrate 'StoreDataFlowNodeState'

--- data-common-jee-store/src/main/java/com/arjuna/databroker/data/jee/store/DataFlowNodeUtils.java
+++ data-common-jee-store/src/main/java/com/arjuna/databroker/data/jee/store/DataFlowNodeUtils.java
@@ -4,6 +4,9 @@
 
 package com.arjuna.databroker.data.jee.store;
 
+import java.io.Serializable;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -19,6 +22,50 @@ import javax.persistence.PersistenceContext;
 public class DataFlowNodeUtils
 {
     private static final Logger logger = Logger.getLogger(DataFlowNodeUtils.class.getName());
+
+    public void create(String id, String name, Map<String, String> properties, String nodeClassName)
+    {
+        DataFlowNodeEntity dataFlowNodeEntity = new DataFlowNodeEntity(id, name, properties, nodeClassName);
+
+        _entityManager.persist(dataFlowNodeEntity);
+    }
+
+    public Serializable getState(String id)
+    {
+        DataFlowNodeEntity dataFlowNodeEntity = _entityManager.find(DataFlowNodeEntity.class, id);
+
+        if (dataFlowNodeEntity != null)
+            return dataFlowNodeEntity.getState();
+        else
+        {
+            logger.log(Level.WARNING, "Get State: Unable to find Data Flow Node Entity");
+            return null;
+        }
+    }
+
+    public void setState(String id, Serializable state)
+    {
+        DataFlowNodeEntity dataFlowNodeEntity = _entityManager.find(DataFlowNodeEntity.class, id);
+
+        if (dataFlowNodeEntity != null)
+        {
+            dataFlowNodeEntity.setState(state);
+
+            _entityManager.merge(dataFlowNodeEntity);
+        }
+        else
+            logger.log(Level.WARNING, "Set State: Unable to find Data Flow Node Entity");
+    }
+
+    public void remove(String id)
+    {
+        DataFlowNodeEntity dataFlowNodeEntity  = _entityManager.find(DataFlowNodeEntity.class, id);
+
+        if (dataFlowNodeEntity != null)
+            _entityManager.remove(dataFlowNodeEntity);
+        else
+            logger.log(Level.WARNING, "Remove: Unable to find Data Flow Node Entity");
+    }
 
     @PersistenceContext(unitName="Data")
     private EntityManager _entityManager;
