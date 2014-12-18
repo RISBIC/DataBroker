584cba7a0e26db3f70565c5e5a855760cce6eb67
Stuart Wheater <stuart.wheater@arjuna.com>
2014-12-18 16:10:19 +0000

    Pre-restructure for durability

--- data-common-jee-store/src/main/java/com/arjuna/databroker/data/jee/store/DataFlowUtils.java
+++ data-common-jee-store/src/main/java/com/arjuna/databroker/data/jee/store/DataFlowUtils.java
@@ -4,6 +4,9 @@
 
 package com.arjuna.databroker.data.jee.store;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -19,6 +22,33 @@ import javax.persistence.PersistenceContext;
 public class DataFlowUtils
 {
     private static final Logger logger = Logger.getLogger(DataFlowUtils.class.getName());
+
+    public void create(String id, String name, Map<String, String> properties)
+    {
+        DataFlowEntity dataFlowEntity = new DataFlowEntity(id, name, properties, Collections.<DataFlowNodeEntity>emptySet(), Collections.<DataFlowLinkEntity>emptySet());
+
+        _entityManager.persist(dataFlowEntity);
+    }
+
+    public DataFlowEntity get(String id)
+    {
+        DataFlowEntity dataFlowEntity = _entityManager.find(DataFlowEntity.class, id);
+
+        if (dataFlowEntity == null)
+            logger.log(Level.WARNING, "Set State: Unable to find Data Flow Entity");
+
+        return dataFlowEntity;
+    }
+
+    public void remove(String id)
+    {
+        DataFlowEntity dataFlowEntity = _entityManager.find(DataFlowEntity.class, id);
+
+        if (dataFlowEntity != null)
+            _entityManager.remove(dataFlowEntity);
+        else
+            logger.log(Level.WARNING, "Remove: Unable to find Data Flow Entity");
+    }
 
     @PersistenceContext(unitName="Data")
     private EntityManager _entityManager;
