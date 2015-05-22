6eef033aa6ac048e038f2fb24381682e52102f7e
markturner <mail.mdturner@gmail.com>
2015-05-22 10:46:24 +0100

    Chagnes to licence labelling and error message positioning. New favicon. Disabled upload button when uplaod is underway.

--- publishingportal-root/src/main/app/licences/controllers/licences.controller.js
+++ publishingportal-root/src/main/app/licences/controllers/licences.controller.js
@@ -235,12 +235,15 @@ angular.module('licences').controller('LicencesController', ['$scope', '$state',
             url: endpoint.value,
             file: file
           }).progress(function (evt) {
+            $scope.uploadInProgress = true;
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
             console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
           }).success(function (data, status, headers, config) {
+            $scope.uploadInProgress = false;
             console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
             $scope.uploadSuccess = true;
           }).error(function(data, status, headers, config){
+            $scope.uploadInProgress = false;
             $scope.uploadFailed = true;
           });
         }
