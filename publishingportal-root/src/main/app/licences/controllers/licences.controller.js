a4f8f91e5d2ff130668ca17c8217f6ed05c25efd
markturner <mail.mdturner@gmail.com>
2015-06-02 12:50:01 +0100

    Pagination of current licences

--- publishingportal-root/src/main/app/licences/controllers/licences.controller.js
+++ publishingportal-root/src/main/app/licences/controllers/licences.controller.js
@@ -6,6 +6,10 @@ angular.module('licences').controller('LicencesController', ['$scope', '$state',
     $scope.$state = $state;
     $scope.uploadSuccess = false;
     $scope.uploadFailed = false;
+    $scope.licences = [];
+
+    $scope.itemsPerPage = 10;
+    $scope.currentPage = 0;
 
     var templates = [];
 
@@ -257,5 +261,40 @@ angular.module('licences').controller('LicencesController', ['$scope', '$state',
       $state.go('licences');
 
     };
+
+    $scope.prevPage = function() {
+      if ($scope.currentPage > 0) {
+        $scope.currentPage--;
+      }
+    };
+
+    $scope.prevPageDisabled = function() {
+      return $scope.currentPage === 0 ? "disabled" : "";
+    };
+
+    $scope.pageCount = function() {
+      return Math.ceil($scope.licences.length/$scope.itemsPerPage)-1;
+    };
+
+    $scope.nextPage = function() {
+      if ($scope.currentPage < $scope.pageCount()) {
+        $scope.currentPage++;
+      }
+    };
+
+    $scope.nextPageDisabled = function() {
+      return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+    };
   }
 ]);
+
+angular.module('licences').filter('offset', function() {
+  return function(input, start) {
+
+    console.log(input);
+    console.log(start);
+
+    start = parseInt(start, 10);
+    return input.slice(start);
+  };
+});
